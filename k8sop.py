5d8107c8d63117b16e243e8e6f758e065fd83c75
mbadda.khoury@infinitylabs.co.il <mbadda.khoury@infinitylabs.co.il>
2025-06-18 22:11:34 +0300

    updated airflow image to use a custom built image

--- k8sop.py
+++ k8sop.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
 
 with DAG(
