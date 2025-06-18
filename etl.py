5e4ee65721e16223c8169a54027f21802f5c02f8
mbadda.khoury@infinitylabs.co.il <mbadda.khoury@infinitylabs.co.il>
2025-06-18 22:40:21 +0300

    added etl dag

--- etl.py
+++ etl.py
@@ -3,40 +3,59 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import Kubernete
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 
-from kubernetes.client import models as k8s
 from kubernetes import client, config
 
-import datetime
+PVC_NAME = "monday-etl-pvc"
+PVC_SIZE = "1Gi"
+PVC_NAMESPACE = "airflow"  # Change this if your Airflow runs in a different namespace
 
 default_args = {
     'start_date': days_ago(1),
 }
 
-PVC_NAME = "monday-etl-pvc"
+def create_pvc():
+    config.load_incluster_config()
+    v1 = client.CoreV1Api()
+
+    pvc = client.V1PersistentVolumeClaim(
+        metadata=client.V1ObjectMeta(name=PVC_NAME),
+        spec=client.V1PersistentVolumeClaimSpec(
+            access_modes=["ReadWriteOnce"],
+            resources=client.V1ResourceRequirements(
+                requests={"storage": PVC_SIZE}
+            )
+        )
+    )
+    v1.create_namespaced_persistent_volume_claim(namespace=PVC_NAMESPACE, body=pvc)
 
 def delete_pvc():
     config.load_incluster_config()
-    core_v1 = client.CoreV1Api()
-    core_v1.delete_namespaced_persistent_volume_claim(
+    v1 = client.CoreV1Api()
+    v1.delete_namespaced_persistent_volume_claim(
         name=PVC_NAME,
-        namespace="airflow",  # Change if using another namespace
+        namespace=PVC_NAMESPACE,
         body=client.V1DeleteOptions()
     )
 
 with DAG(
-    dag_id="monday_k8s_etl_dag",
+    dag_id="monday_k8s_etl_dag_dynamic_pvc",
     default_args=default_args,
     schedule_interval=None,
     catchup=False,
-    tags=["kubernetes", "etl"]
+    tags=["kubernetes", "etl"],
 ) as dag:
 
-    volume = k8s.V1Volume(
-        name="etl-storage",
-        persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name=PVC_NAME),
+    create_pvc_task = PythonOperator(
+        task_id="create_pvc",
+        python_callable=create_pvc,
     )
 
-    volume_mount = k8s.V1VolumeMount(
+    volume = client.V1Volume(
+        name="etl-storage",
+        persistent_volume_claim=client.V1PersistentVolumeClaimVolumeSource(claim_name=PVC_NAME),
+    )
+
+    volume_mount = client.V1VolumeMount(
         mount_path="/app/boards",
         name="etl-storage",
         read_only=False,
@@ -50,7 +69,7 @@ with DAG(
         volume_mounts=[volume_mount],
         get_logs=True,
         is_delete_operator_pod=True,
-        namespace="airflow",
+        namespace=PVC_NAMESPACE,
     )
 
     transformer = KubernetesPodOperator(
@@ -61,12 +80,13 @@ with DAG(
         volume_mounts=[volume_mount],
         get_logs=True,
         is_delete_operator_pod=True,
-        namespace="airflow",
+        namespace=PVC_NAMESPACE,
     )
 
-    cleanup_pvc = PythonOperator(
+    delete_pvc_task = PythonOperator(
         task_id="delete_pvc",
-        python_callable=delete_pvc
+        python_callable=delete_pvc,
+        trigger_rule="all_done",  # Ensure PVC is deleted even if previous tasks fail
     )
 
-    extractor >> transformer >> cleanup_pvc
+    create_pvc_task >> extractor >> transformer >> delete_pvc_task
